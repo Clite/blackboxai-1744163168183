7e9a954d688f5dae392910efb67cde70d70c4af8
Clite <mutalexzw@gmail.com>
2025-04-09 03:47:53 +0200

    Add index.php 1744163273033

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'config/database.php';
+require_once 'includes/header.php';
+?>
+
+<h1>Welcome to POS System</h1>
+<p>Select a module from the navigation menu to get started.</p>
+
+<?php
+require_once 'includes/footer.php';
+?>

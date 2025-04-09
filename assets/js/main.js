bdd136439eb89d59fb52bbc0763e850c3435a9a8
Clite <mutalexzw@gmail.com>
2025-04-09 03:47:51 +0200

    Add assets/js/main.js 1744163271398

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize tooltips
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+
+    // Confirm before delete actions
+    const deleteButtons = document.querySelectorAll('.btn-delete');
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', function(e) {
+            if (!confirm('Are you sure you want to delete this item?')) {
+                e.preventDefault();
+            }
+        });
+    });
+
+    // POS system specific functions
+    function calculateTotal() {
+        // Will be implemented in sales module
+    }
+
+    function updateInventory() {
+        // Will be implemented in inventory module
+    }
+});
